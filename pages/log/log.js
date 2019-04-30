d97f4f2d37f021eb4bd5013da6d760c26365962a
bushitan <373514952@qq.com>
2019-04-30 20:20:17 +0800

    1.2.6

--- pages/log/log.js
+++ pages/log/log.js
@@ -12,6 +12,9 @@ Page({
         menu: ['积分', '奖品', '兑换券'],
         isLoading: true,
         prizeList: [],
+        pageNum:1,
+        range:10,
+        showModel:0,
     },
 
     /**
@@ -19,103 +22,35 @@ Page({
      */
     onLoad: function (options) {
         GP = this
-        GP.getLog()
-        // GP.getStoreDetail(options)
-        // action_score.getScorePrize(wx.getStorageSync(API.USER_ID)).then(res => {
-        //     console.log(res)
-        //     GP.setData({
-        //         isLoading:false,
-        //         scoreList: res.score.data,
-        //         prizeList: res.prize.data,
-        //         shareList: res.share.data
-        //     })
-        // })
+        GP.getLog("score")
     },
 
-    async getLog(){
+    async getLog(model){
         var dataList = await db.storeDataSeller({
-            model:"score",
-            page_num:1,
-            range:10,
+            model: model,
+            page_num: GP.data.pageNum,
+            range: GP.data.range,
         })
         GP.setData({
             isLoading: false,
             detailList: dataList,
-            // store: store,
         })
-
-        // console.log(dataList)
     },
+    
 
-
-
-    async getStoreDetail(options) {
-        var store_uuid = options.store_uuid
-        detailList = await db.storeDetail(
-            "share",
-            store_uuid
-        )
-
-        var pages = getCurrentPages()
-        var prevPage = pages[pages.length - 2]
-        var store = prevPage.data.store
+    click(e){
+        var index = e.detail
+        console.log(index)
+        var model
+        if (index == 0) model = "score"
+        else if (index == 1) model = "prize"
+        else model = "share"
         GP.setData({
-            isLoading: false,
-            detailList: detailList,
-            store: store,
+            showModel:index,
         })
-
-        // // console.log(list)
-        // detail = await db.storeDetail(store_uuid)
-
-        // GP.setData({
-        //     store: store,
-        //     detail: detail
-        // })
+        GP.getLog(model)
     },
 
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
 
     /**
      * 用户点击右上角分享
